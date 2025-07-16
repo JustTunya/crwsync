d9190e05c9b8fe3f6a3cdf892bf89e9027e008ea
Tunya <tunyalenard03@gmail.com>
2025-07-16 23:01:05 +0300

    added CORS to backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://crwsync.com', 'https://www.crwsync.com', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
