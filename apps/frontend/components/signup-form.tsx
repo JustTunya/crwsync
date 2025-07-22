6219a9a0aa2dc7c78a7d69ec031b0f655d70f8af
Tunya <tunyalenard03@gmail.com>
2025-07-22 10:46:54 +0300

    UI updates and added color scheme

--- apps/frontend/components/signup-form.tsx
+++ apps/frontend/components/signup-form.tsx
@@ -80,101 +80,105 @@ export function SignupForm() {
   };
 
   return (
-    <div>
-      <form action={formAction} className="space-y-6">
-        <div className="space-y-4">
-          <Label htmlFor="email">Email Address</Label>
+    <div className="max-w-md mx-auto p-8 bg-white/40 backdrop-blur-md border border-white/50 shadow-xl/5 rounded-2xl">
+    <form action={formAction} className="space-y-6">
+      <div className="space-y-3">
+        <Label htmlFor="email">Email Address</Label>
+        <Input
+          id="email"
+          type="email"
+          value={email}
+          validation={checkEmail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {!checkEmail && (
+          <Label className="text-red-500">Email is already taken</Label>
+        )}
+      </div>
+
+      <div className="space-y-3">
+        <Label htmlFor="username">Username</Label>
+        <Input
+          id="username"
+          type="text"
+          value={username}
+          validation={checkUsername}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        {!checkUsername && (
+          <Label className="text-red-500">Username is already taken</Label>
+        )}
+      </div>
+
+      <div className="space-y-3">
+        <Label htmlFor="phone">Phone Number</Label>
+
+        <div className="flex flex-row items-center space-x-2">
+          <Select value={dial} onValueChange={onSelectCountry}>
+            <SelectTrigger>
+              <SelectValue>
+                {(() => {
+                  const selection = countrylist.find((c) => c.iso3166.alpha2 === dial);
+                  if (!selection) return <span>Select Country</span>;
+                  return (
+                    <>
+                      <div className="w-6 h-4 relative">
+                        <Image
+                          src={selection.flag}
+                          alt={selection.iso3166.alpha2}
+                          fill
+                          className="object-cover rounded-[3px] shadow-xs"
+                          onError={(e) => {
+                            // Fallback to a placeholder if the flag image fails to load
+                            e.currentTarget.src = "/flags/placeholder.svg";
+                          }}
+                        />
+                        <div className="absolute inset-0 rounded-[3px] shadow-[inset_0_4px_4px_-2px_rgba(255,255,255,0.5),inset_0_-1px_3px_-1px_rgba(0,0,0,0.4)]" />
+                      </div>
+                      <p className="text-sm font-medium">{selection.dial_code}</p>
+                    </>
+                  );
+                })()}
+              </SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              {countrylist.filter((c) => c.dial_code).sort((a, b) => a.iso3166.alpha2.localeCompare(b.iso3166.alpha2)).map((c) => (
+                <SelectItem key={c.iso3166.alpha2} value={c.iso3166.alpha2}>
+                  <div className="w-6 h-4 relative">
+                    <Image 
+                      src={c.flag} 
+                      alt={c.iso3166.alpha2} 
+                      fill 
+                      className="object-cover rounded-[4px] shadow-md"
+                      onError={(e) => {
+                        // Fallback to a placeholder if the flag image fails to load
+                        e.currentTarget.src = "/flags/placeholder.svg";
+                      }}
+                    />
+                    <div className="absolute inset-0 rounded-[4px] shadow-[inset_0_4px_4px_-2px_rgba(255,255,255,0.5),inset_0_-1px_3px_-1px_rgba(0,0,0,0.4)]" />
+                  </div>
+                  <p className="text-sm font-medium">{c.iso3166.alpha2}</p>
+                  <p className="text-xs text-blue-800 bg-blue-800/10 px-1 pb-0.5 pt-0 rounded-sm">{c.dial_code}</p>
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+
           <Input
-            id="email"
-            type="email"
-            value={email}
-            validation={checkEmail}
-            onChange={(e) => setEmail(e.target.value)}
+            id="phone"
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
           />
-          {!checkEmail && (
-            <Label className="text-red-500">Email is already taken</Label>
-          )}
         </div>
+        {true && (
+          <Label className="text-red-500">Please provide a valid phone number</Label>
+        )}
+      </div>
 
-        <div className="space-y-4">
-          <Label htmlFor="username">Username</Label>
-          <Input
-            id="username"
-            type="text"
-            value={username}
-            validation={checkUsername}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          {!checkUsername && (
-            <Label className="text-red-500">Username is already taken</Label>
-          )}
-        </div>
-
-        <div className="space-y-4">
-          <Label htmlFor="phone">Phone Number</Label>
-
-          <div className="flex flex-row items-center space-x-2">
-            <Select value={dial} onValueChange={onSelectCountry}>
-              <SelectTrigger>
-                <SelectValue>
-                  {(() => {
-                    const selection = countrylist.find((c) => c.iso3166.alpha2 === dial);
-                    if (!selection) return <span>Select Country</span>;
-                    return (
-                      <>
-                        <div className="w-6 h-4 relative">
-                          <Image
-                            src={selection.flag}
-                            alt={selection.iso3166.alpha2}
-                            fill
-                            className="object-cover rounded-[3px] shadow-xs"
-                            onError={(e) => {
-                              // Fallback to a placeholder if the flag image fails to load
-                              e.currentTarget.src = "/flags/placeholder.svg";
-                            }}
-                          />
-                          <div className="absolute inset-0 rounded-[3px] shadow-[inset_0_4px_4px_-2px_rgba(255,255,255,0.5),inset_0_-1px_3px_-1px_rgba(0,0,0,0.4)]" />
-                        </div>
-                        <p className="text-sm font-medium">{selection.dial_code}</p>
-                      </>
-                    );
-                  })()}
-                </SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                {countrylist.filter((c) => c.dial_code).sort((a, b) => a.iso3166.alpha2.localeCompare(b.iso3166.alpha2)).map((c) => (
-                  <SelectItem key={c.iso3166.alpha2} value={c.iso3166.alpha2}>
-                    <div className="w-6 h-4 relative">
-                      <Image 
-                        src={c.flag} 
-                        alt={c.iso3166.alpha2} 
-                        fill 
-                        className="object-cover rounded-[4px] shadow-md"
-                        onError={(e) => {
-                          // Fallback to a placeholder if the flag image fails to load
-                          e.currentTarget.src = "/flags/placeholder.svg";
-                        }}
-                      />
-                      <div className="absolute inset-0 rounded-[4px] shadow-[inset_0_4px_4px_-2px_rgba(255,255,255,0.5),inset_0_-1px_3px_-1px_rgba(0,0,0,0.4)]" />
-                    </div>
-                    <p className="text-sm font-medium">{c.iso3166.alpha2}</p>
-                    <p className="text-xs text-blue-800 bg-blue-800/10 px-1 pb-0.5 pt-0 rounded-sm">{c.dial_code}</p>
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-
-            <Input
-              id="phone"
-              type="tel"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-row space-x-4">
-          <div className="space-y-4">
+      <div className="space-y-3">
+        <div className="flex flex-row space-x-3">
+          <div className="space-y-3">
             <Label htmlFor="firstname">First Name</Label>
             <Input
               id="firstname"
@@ -184,7 +188,7 @@ export function SignupForm() {
             />
           </div>
 
-          <div className="space-y-4">
+          <div className="space-y-3">
             <Label htmlFor="lastname">Last Name</Label>
             <Input
               id="lastname"
@@ -194,67 +198,74 @@ export function SignupForm() {
             />
           </div>
         </div>
+        {true && (
+          <Label className="text-red-500">Please provide a valid first and last name</Label>
+        )}
+      </div>
 
-        <div className="space-y-4">
-          <Label htmlFor="birthdate">Birthdate</Label>
-          <Popover open={openCalendar} onOpenChange={setOpenCalendar}>
-            <PopoverTrigger asChild className="bg-transparent">
-              <Button
-                variant="outline"
-                className="w-full justify-between"
-                onClick={() => setOpenCalendar(!openCalendar)}
-              >
-                {birthdate ? birthdate.toISOString().split("T")[0] : "Select Birthdate"}
-                <HugeiconsIcon icon={Calendar04Icon} size={18} strokeWidth={1} className="ml-2" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={birthdate}
-                captionLayout="dropdown"
-                onSelect={(date) => {
-                  if (date) {
-                    setBirthdate(date);
-                    setOpenCalendar(false);
-                  }
-                }}
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
+      <div className="space-y-3">
+        <Label htmlFor="birthdate">Birthdate</Label>
+        <Popover open={openCalendar} onOpenChange={setOpenCalendar}>
+          <PopoverTrigger asChild className="bg-white/60 backdrop-blur-md border border-white/50 shadow-lg/5 font-normal">
+            <Button
+              variant="outline"
+              className="w-full justify-between"
+              onClick={() => setOpenCalendar(!openCalendar)}
+            >
+              {birthdate ? birthdate.toISOString().split("T")[0] : "Select Birthdate"}
+              <HugeiconsIcon icon={Calendar04Icon} size={18} strokeWidth={1} className="ml-2" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode="single"
+              selected={birthdate}
+              captionLayout="dropdown"
+              onSelect={(date) => {
+                if (date) {
+                  setBirthdate(date);
+                  setOpenCalendar(false);
+                }
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+        {true && (
+          <Label className="text-red-500">Please provide a valid birthdate</Label>
+        )}
+      </div>
 
-        <div className="space-y-4">
-          <Label htmlFor="password">Password</Label>
-          <Input
-            id="password"
-            type="password"
-            value={password}
-            visible={showPass}
-            setVisible={() => setShowPass(!showPass)}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+      <div className="space-y-3">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          id="password"
+          type="password"
+          value={password}
+          visible={showPass}
+          setVisible={() => setShowPass(!showPass)}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
 
-        <div className="space-y-4">
-          <Label htmlFor="confpassword">Confirm Password</Label>
-          <Input
-            id="confpassword"
-            type="password"
-            value={confpassword}
-            visible={showConfPass}
-            setVisible={() => setShowConfPass(!showConfPass)}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </div>
+      <div className="space-y-3">
+        <Label htmlFor="confpassword">Confirm Password</Label>
+        <Input
+          id="confpassword"
+          type="password"
+          value={confpassword}
+          visible={showConfPass}
+          setVisible={() => setShowConfPass(!showConfPass)}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </div>
 
-        <Button
-          type="submit"
-          disabled={pending}
-        >
-          {pending ? "Creating account..." : "Create Account"}
-        </Button>
-      </form>
+      <Button
+        type="submit"
+        disabled={pending}
+      >
+        {pending ? "Creating account..." : "Create Account"}
+      </Button>
+    </form>
     </div>
   );
 }
--- apps/frontend/components/ui/input.tsx
+++ apps/frontend/components/ui/input.tsx
@@ -18,12 +18,12 @@ function Input({ className, type, visible, setVisible, validation, ...props }: I
   const iconWidth = 1;
 
   return (
-    <div className="w-full flex justify-items-center border border-input shadow-xs rounded-md">
+    <div className="w-full flex justify-items-center bg-white/60 backdrop-blur-md border border-white/50 shadow-lg/5 rounded-md">
       <input
         type={type === "password" && visible ? "text" : type}
         data-slot="input"
         className={cn(
-          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 flex h-9 w-full min-w-0 bg-transparent px-3 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "file:text-foreground placeholder:text-muted-foreground selection:text-black selection:bg-gray-200 dark:bg-input/30 flex h-9 w-full min-w-0 px-3 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           //"focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
           "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
           className
@@ -34,6 +34,7 @@ function Input({ className, type, visible, setVisible, validation, ...props }: I
         {hasVisibilityIcon && <HugeiconsIcon icon={visible ? ViewOffSlashIcon : ViewIcon} size={iconSize} strokeWidth={iconWidth} onClick={setVisible} className="cursor-pointer" />}
         {hasValidationIcon && <HugeiconsIcon icon={validation ? CheckmarkCircle02Icon : CancelCircleIcon} size={iconSize} strokeWidth={iconWidth} />}
       </div>
+      {/* <div className="absolute inset-0 rounded-md shadow-[inset_0_4px_8px_0_rgba(255,255,255,0.4),inset_0_-1px_6px_-1px_rgba(0,0,0,0.1)]" /> */}
     </div>
   )
 }
