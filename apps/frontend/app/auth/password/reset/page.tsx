06c745eb3af03496aa95b5cb58e119ed1aeb7500
Tunya <tunyalenard03@gmail.com>
2025-07-31 13:27:53 +0300

    fixed frontend build errors

--- apps/frontend/app/auth/password/reset/page.tsx
+++ apps/frontend/app/auth/password/reset/page.tsx
@@ -1,9 +1,12 @@
 import { ResetPasswordForm } from "@/components/reset-password-form";
+import { Suspense } from "react";
 
 export default function ResetPasswordPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen space-y-6">
-      <ResetPasswordForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordForm />
+      </Suspense>
     </div>
   );
 }
