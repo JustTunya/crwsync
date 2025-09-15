39b89f66e94a8c39daab56b35eaa117d73501957
Tunya <tunyalenard03@gmail.com>
2025-09-15 11:26:39 +0300

    updated entrypoint

--- apps/backend/entrypoint.sh
+++ apps/backend/entrypoint.sh
@@ -3,12 +3,9 @@ set -e
 i=0
 until node apps/backend/dist/migrate.js; do
   i=$((i+1))
-  if [ "$i" -gt 10 ]; then
-    echo "Postgres is still not available after 10 attempts, exiting."
-    exit 1
-  fi
-  echo "Waiting for Postgres..."
-  sleep 1
+  [ "$i" -ge 10 ] && echo "migrations failed after $i attempts" && exit 1
+  echo "migrations failed, retrying in 5 seconds..."
+  sleep 5
 done
 
 exec node apps/backend/dist/main.js
