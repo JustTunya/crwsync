fdb8d4f0bff544b42acb12ef9fc7bf9fa307ff53
Tunya <tunyalenard03@gmail.com>
2025-09-24 22:05:55 +0300

    fixed signup page state logic

--- apps/frontend/components/signup-step1.tsx
+++ apps/frontend/components/signup-step1.tsx
@@ -21,18 +21,13 @@ interface SignupStep1Props {
 }
 
 export default function SignupStep1(props: SignupStep1Props) {
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confpassword, setConfirmPassword] = useState("");
-
   const [showPass, setShowPass] = useState(false);
   const [showConfPass, setShowConfPass] = useState(false);
 
-  const validEmail = useAvailability("email", email, isEmailValid);
-  const validUsername = useAvailability("username", username, isUsernameValid);
-  const validPassword = useValidator(password, isPasswordStrong);
-  const matchingPasswords = useMatch(password, confpassword);
+  const validEmail = useAvailability("email", props.form.email, isEmailValid);
+  const validUsername = useAvailability("username", props.form.username, isUsernameValid);
+  const validPassword = useValidator(props.form.password, isPasswordStrong);
+  const matchingPasswords = useMatch(props.form.password, props.form.confpassword);
 
   const validStep = useMemo(() => {
     return (
@@ -43,26 +38,6 @@ export default function SignupStep1(props: SignupStep1Props) {
     );
   }, [validEmail, validUsername, validPassword, matchingPasswords]);
 
-  const handleEmailChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    props.updateForm("email", e.target.value);
-  }, [props]);
-
-  const handleUsernameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-    props.updateForm("username", e.target.value);
-  }, [props]);
-
-  const handlePasswordChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-    props.updateForm("password", e.target.value);
-  }, [props]);
-
-  const handleConfirmPasswordChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setConfirmPassword(e.target.value);
-    props.updateForm("confpassword", e.target.value);
-  }, [props]);
-
   return(
     <>
       <div className="space-y-4">
@@ -70,10 +45,10 @@ export default function SignupStep1(props: SignupStep1Props) {
         <Input
           id="email"
           type="email"
-          value={email}
+          value={props.form.email}
           placeholder="johndoe@example.com"
           validation={validEmail?.available}
-          onChange={handleEmailChange}
+          onChange={(e) => props.updateForm("email", e.target.value)}
           error={validEmail?.available === false}
           autoFocus
         />
@@ -87,10 +62,10 @@ export default function SignupStep1(props: SignupStep1Props) {
         <Input
           id="username"
           type="text"
-          value={username}
+          value={props.form.username}
           placeholder="johndoe"
           validation={validUsername?.available}
-          onChange={handleUsernameChange}
+          onChange={(e) => props.updateForm("username", e.target.value)}
           error={validUsername?.available === false}
         />
         {(validUsername?.available === false && validUsername?.message) && (
@@ -103,10 +78,10 @@ export default function SignupStep1(props: SignupStep1Props) {
         <Input
           id="password"
           type="password"
-          value={password}
+          value={props.form.password}
           visible={showPass}
           setVisible={() => setShowPass(!showPass)}
-          onChange={handlePasswordChange}
+          onChange={(e) => props.updateForm("password", e.target.value)}
           error={matchingPasswords === false && validPassword?.value === true}
         />
       </div>
@@ -130,10 +105,10 @@ export default function SignupStep1(props: SignupStep1Props) {
         <Input
           id="confpassword"
           type="password"
-          value={confpassword}
+          value={props.form.confpassword}
           visible={showConfPass}
           setVisible={() => setShowConfPass(!showConfPass)}
-          onChange={handleConfirmPasswordChange}
+          onChange={(e) => props.updateForm("confpassword", e.target.value)}
           error={matchingPasswords === false && validPassword?.value === true}
         />
         {(matchingPasswords === false) && (
