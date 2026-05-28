4359ee3b229b6b204a8b9002431d611f9d576f03
Tunya <tunyalenard03@gmail.com>
2026-05-28 11:45:25 +0300

    changed global modules in the nav sidebar

---
+++ apps/frontend/dash/app/[slug]/statistics/page.tsx
@@ -0,0 +1,8 @@
+export default function StatsPage() {
+  return (
+    <div className="size-full flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-semibold mb-4">Statistics</h1>
+      <p>This is the statistics page.</p>
+    </div>
+  );
+}
